78d92442e605bc87cbf87db09117bb8cff266042
Arweenn <arwendumont25@outlook.fr>
2024-04-16 14:21:35 +0200

    task 2

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that uses a REST API to return informations
+about an employee's progress.
+"""
+
+import requests
+from sys import argv
+import json
+
+
+if __name__ == "__main__":
+
+    user_id = argv[1]
+
+    infos = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(user_id))
+    user = infos.json()
+    EMPLOYEE_NAME = user.get("username")
+
+    infos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userID={}".format(user_id))
+    todos = infos.json()
+
+    tasks = []
+    for todo in todos:
+        task = {
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": EMPLOYEE_NAME
+        }
+        tasks.append(task)
+
+    data = {user_id: tasks}
+    with open("{}.json".format(user_id), "w") as js_f:
+        js_f.write(json.dumps(data))
