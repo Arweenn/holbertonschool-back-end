2547737e2c71946bc27529d4fc55227e809dad12
Arweenn <arwendumont25@outlook.fr>
2024-04-16 10:48:40 +0200

    pycode fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Script that uses a REST API to return informations
-about an employee's progress. 
+about an employee's progress.
 """
 
 import requests
@@ -12,19 +12,21 @@ if __name__ == "__main__":
 
     user_id = argv[1]
 
-    infos = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
+    infos = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
     user = infos.json()
     EMPLOYEE_NAME = user.get("name")
 
-    infos = requests.get('https://jsonplaceholder.typicode.com/todos?userID={}'.format(user_id))
+    infos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userID={}'.format(user_id))
     todos = infos.json()
     TOTAL_NUMBER_OF_TASKS = len(todos)
 
-    NUMBER_OF_DONE_TASKS = [task for task in todos if task.get("completed") is True]
+    NUMBER_OF_DONE_TASKS = [task for task
+                            in todos if task.get("completed") is True]
 
     print("Employee {} is done with task({}/{}):".format
           (EMPLOYEE_NAME, len(NUMBER_OF_DONE_TASKS), TOTAL_NUMBER_OF_TASKS))
 
     for task in NUMBER_OF_DONE_TASKS:
-        title = task.get("title")
-        print("\t {}".format(title))
+        print("\t {}".format(task.get("title")))
